b620fc0628992a28456d836fe4e0c73ff6be8e51
Ramanarayana <gunjirama1999@gmail.com>
2023-06-03 15:05:23 +0530

    catalogue_colours _implemeting_V1

--- redis.sh
+++ redis.sh
@@ -8,8 +8,8 @@ yum module enable redis:remi-6.2 -y
 echo -e "\e[31m enable  redis \e[0m"
 yum install redis -y
 echo -e "\e[31m>>>>>>>>>>>>>>>>>>>>>>>> updateing redis listen address <<<<<<<<<<<< \e[0m"
-sed  -i -e 's|127.0.0.1|0.0.0.0|' /etc/redis.conf
-
+sed -i -e 's|127.0.0.1 | 0.0.0.0|' /etc/redis.conf /etc/redis/redis.conf
+### to check the status is netstat-lntp
 echo -e "\e[31m<<<<<<<<<<<<<<<<<<<<<<< enable  redis >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>\e[0m"
 systemctl enable redis
 systemctl start redis
