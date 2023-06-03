b06b4f7e33682bf755e351ad4431531b2a99589f
Ramanarayana <gunjirama1999@gmail.com>
2023-06-03 16:34:20 +0530

    dispatch_Intellij_code_1

---
+++ dispatch.sh
@@ -0,0 +1,23 @@
+echo -e "\e[31m<<<<<<<<<<<<<<<<<<<<<<< install  GOlang >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>\e[0m"m
+yum install golang -y
+echo -e "\e[31m<<<<<<<<<<<<<<<<<<<<<<< Add application useras roboshop >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>\e[0m"m
+useradd roboshop
+rm -rf /app
+mkdir /app
+echo -e "\e[31m<<<<<<<<<<<<<<<<<<<<<<< download roboshop-artifacts >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>\e[0m"m
+curl -L -o /tmp/dispatch.zip https://roboshop-artifacts.s3.amazonaws.com/dispatch.zip
+echo -e "\e[31m<<<<<<<<<<<<<<<<<<<<<<< extract data from file >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>\e[0m"m
+cd /app
+unzip /tmp/dispatch.zip
+echo -e "\e[31m<<<<<<<<<<<<<<<<<<<<<<< dispatching >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>\e[0m"m
+
+go mod init dispatch
+go get
+go build
+echo -e "\e[31m<<<<<<<<<<<<<<<<<<<<<<< start services >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>\e[0m"m
+cp /root/roboshop-shell/dispatch.service /etc/systemd/system/dispatch.service
+echo -e "\e[31m<<<<<<<<<<<<<<<<<<<<<<< start services >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>\e[0m"m
+systemctl daemon-reload
+
+systemctl enable dispatch
+systemctl start dispatch
