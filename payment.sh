d0089979631f393a9e46b92f38c3440658884387
Ramanarayana <gunjirama1999@gmail.com>
2023-06-03 16:22:32 +0530

    Payment_Intellij_code_1

---
+++ payment.sh
@@ -0,0 +1,20 @@
+echo -e "\e[31m<<<<<<<<<<<<<<<<<<<<<<< install python >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>\e[0m"m
+yum install python36 gcc python3-devel -y
+echo -e "\e[31m<<<<<<<<<<<<<<<<<<<<<<< Add application user >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>\e[0m"m
+useradd roboshop
+rm -rf /app
+mkdir /app
+echo -e "\e[31m<<<<<<<<<<<<<<<<<<<<<<< Download the app content>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>\e[0m"m
+curl -L -o /tmp/payment.zip https://roboshop-artifacts.s3.amazonaws.com/payment.zip
+echo -e "\e[31m<<<<<<<<<<<<<<<<<<<<<<< install maven >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>\e[0m"m
+cd /app
+unzip /tmp/payment.zip
+echo -e "\e[31m<<<<<<<<<<<<<<<<<<<<<<< install dependencies >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>\e[0m"m
+
+pip3.6 install -r requirements.txt
+echo -e "\e[31m<<<<<<<<<<<<<<<<<<<<<<< start services >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>\e[0m"m
+cp /root/roboshop-shell/payment.service /etc/systemd/system/payment.service
+echo -e "\e[31m<<<<<<<<<<<<<<<<<<<<<<< start payment services>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>\e[0m"m
+systemctl daemon-reload
+systemctl enable payment
+systemctl start payment
