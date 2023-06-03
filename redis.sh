55e00e79aebec4990728b5abf73135b6fb4f128b
Ramanarayana <gunjirama1999@gmail.com>
2023-06-03 15:00:37 +0530

    catalogue_colours _implemeting_V1

--- redis.sh
+++ redis.sh
@@ -1,6 +1,15 @@
-yum install https://rpms.remirepo.net/enterprise/remi-release-8.rpm -y
-yum module enable redis:remi-6.2 -y
-yum install redis -y
+echo -e "\e[31m >>>>>>> installing redis repos <<<<<<<<<<<<<<\e[0m"
 
+yum install https://rpms.remirepo.net/enterprise/remi-release-8.rpm -y
+
+echo -e "\e[31m <<<<<<<<<<<<<<<enable  redis>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> \e[0m"
+yum module enable redis:remi-6.2 -y
+
+echo -e "\e[31m enable  redis \e[0m"
+yum install redis -y
+echo -e "\e[31m>>>>>>>>>>>>>>>>>>>>>>>> updateing redis listen address <<<<<<<<<<<< \e[0m"
+sed  -i -e 's|127.0.0.1|0.0.0.0|' /etc/redis.conf
+
+echo -e "\e[31m<<<<<<<<<<<<<<<<<<<<<<< enable  redis >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>\e[0m"
 systemctl enable redis
 systemctl start redis
