46fc4fc96378fa706cdc3aa7a13c6f0cbc62d125
Ramanarayana <gunjirama1999@gmail.com>
2023-06-03 15:42:24 +0530

    Mysql_Intellij_code_1

--- mysql.sh
+++ mysql.sh
@@ -1,8 +1,13 @@
+echo -e "\e[31m<<<<<<<<<<<<<<<<<<<<<<< disable mysql 8 version old version>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>\e[0m"
 yum module disable mysql -y
-cp mysql.repo /etc/yum.repos.d/mysql.repo
-
+echo -e "\e[31m<<<<<<<<<<<<<<<<<<<<<<< copy the file >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>\e[0m"
+cp /root/roboshop-shell/mysql.repo /etc/yum.repos.d/mysql.repo
+echo -e "\e[31m<<<<<<<<<<<<<<<<<<<<<<< install my sql >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>\e[0m"
 yum install mysql-community-server -y
+echo -e "\e[31m<<<<<<<<<<<<<<<<<<<<<<< start services >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>\e[0m"
 systemctl enable mysqld
 systemctl start mysqld
+echo -e "\e[31m<<<<<<<<<<<<<<<<<<<<<<< secure installations >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>\e[0m"m
 mysql_secure_installation --set-root-pass RoboShop@1
+echo -e "\e[31m<<<<<<<<<<<<<<<<<<<<<<< password credentials >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>\e[0m"
 mysql -uroot -pRoboShop@1
