1fd714390c2704ab4a5c7a38ae41b81f51ca57e2
Ramanarayana <gunjirama1999@gmail.com>
2023-06-03 13:10:23 +0530

    Mondodb_changing_ip_address_chnging_from_127 to 0.0.0.

--- mongodb.sh
+++ mongodb.sh
@@ -1,6 +1,10 @@
 cp mongo.repo /etc/yum.repos.d/mongo.repo
 
 yum install mongodb-org -y
+### now we need to change the local address from 127.0.0.0.1 to 0.0.0.0
+### for finding give like netstat -lntp
+sed -i -e 's|127.0.0.1|0.0.0.0|' /etc/mongod.conf
+
 systemctl enable mongod
 systemctl start mongod
 systemctl restart mongod
